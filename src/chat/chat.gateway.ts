c63b8d4dc296acfc393cc9dd92d760577be39d78
Oorpow <309754513@qq.com>
2024-08-06 00:20:25 +0800

    feat: ✨ 实现加入房间、消息发送的ws服务

--- src/chat/chat.gateway.ts
+++ src/chat/chat.gateway.ts
@@ -1,7 +1,52 @@
-import { WebSocketGateway } from '@nestjs/websockets';
+import {
+  MessageBody,
+  SubscribeMessage,
+  WebSocketGateway,
+  WebSocketServer,
+} from '@nestjs/websockets';
+import { Server, Socket } from 'socket.io';
+
 import { ChatService } from './chat.service';
 
-@WebSocketGateway()
+interface JoinPayload {
+  chatroomId: number;
+  joinedUserId: number;
+}
+
+interface MessagePayload {
+  fromUserId: number;
+  chatroomId: number;
+  message: {
+    type: 'text' | 'image';
+    content: string;
+  };
+}
+
+@WebSocketGateway({ cors: { origin: '*' } })
 export class ChatGateway {
   constructor(private readonly chatService: ChatService) {}
+
+  @WebSocketServer() server: Server;
+
+  @SubscribeMessage('joinChat')
+  joinChat(client: Socket, payload: JoinPayload) {
+    const chatName = payload.chatroomId.toString();
+    client.join(chatName);
+
+    // 向群聊广播，xxx加入了群聊
+    this.server.to(chatName).emit('message', {
+      type: 'joinChat',
+      userId: payload.joinedUserId,
+    });
+  }
+
+  @SubscribeMessage('sendMsg')
+  sendMsg(@MessageBody() payload: MessagePayload) {
+    const chatName = payload.chatroomId.toString();
+    this.server.to(chatName).emit('message', {
+      type: 'sendMsg',
+      userId: payload.fromUserId,
+      message: payload.message,
+    });
+  }
 }
