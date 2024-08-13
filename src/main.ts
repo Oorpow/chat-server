9062c64a92c5e590404150eaae0b487ed9a91c76
Oorpow <309754513@qq.com>
2024-08-13 14:32:48 +0800

    feat: ✨ 添加响应拦截器

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { ResponseInterceptor } from './common/interceptor/response.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
+  app.useGlobalInterceptors(new ResponseInterceptor());
   app.setGlobalPrefix('/api');
   await app.listen(3000);
 }
